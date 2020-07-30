f0d76070c53bc3bb9cda8f726b2865dbf664fb21
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-07-30 23:22:19 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -26,7 +26,7 @@ module.exports.run = async (client, message, args) => {
     var members = message.guild.members.cache;
     var embed = new discord.MessageEmbed()
  
-    .setTitle(`**● Server Name:** ${message.member.displayAvatarURL}`,)
+    .setTitle(`_●  Server name: **Fused**_ `,)
     .setTimestamp()
     .setFooter(botConfig.footer)
     .setThumbnail(message.author.displayAvatarURL())
