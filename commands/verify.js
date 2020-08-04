2964fa7156a920b679d47b754d54f99992ae6223
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-08-04 23:13:20 +0200

    Create verify.js

---
+++ commands/verify.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(! message.member.roles.cache.has("740310661997002753")) return message.reply("Je bent al verified!");
+
+    var verified = new discord.MessageEmbed()
+    .setTitle("U bent nu verified, ")
+    .setColor("#3de361")
+    .setFooter("DutchFM Bot")
+    .setTimestamp()
+    .setDescription(`${message.author.username} je bent verified!`)
+    return message.channel.send(verified);
+
+    
+}
+
+module.exports.help = {
+    name: "help"
+}
