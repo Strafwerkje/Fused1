727fddb151d2bf4cfb439812f410b6c75111d9c3
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-08-08 22:47:24 +0200

    d

---
+++ commands/meme.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    const subReddits = ["dankmemes", "meme", "memes"]
+    const random = subReddits[Math.floor(Math.random() * subReddits.length)]
+
+    const img = await randomPuppy(random);
+
+    const memeEmbed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setImage(img)
+    .setTitle(`Your meme. From r/${random}`)
+    .setURL(`https://reddit.com/r/${random}`)
+
+    message.channel.send(memeEmbed);
+}
+
+module.exports.help = {
+    name: "meme"
+}
