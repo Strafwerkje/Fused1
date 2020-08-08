fd999e9e72d625d861cd0a5ecedca7f78d60c8c0
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-08-08 23:21:14 +0200

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,56 +1,55 @@
 const discord = require("discord.js");
+const botConfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 
-    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`Missing permissions to dm this user with the bot.`)
-    sql.get(`SELECT * FROM scores WHERE guildId ="${message.guild.id}"`).then(row => {
-    const prefixtouse = row.prefix
-    const usage = new Discord.RichEmbed()
-            .setColor(0x00A2E8)
-            .setThumbnail(client.user.avatarURL)
-            .setTitle("Command: " + prefixtouse + "dm")
-            .addField("Usage", prefixtouse + "dm @Someone <message>")
-            .addField("Example", prefixtouse + "dm @Dowin Hey can you help me with something?")
-            .setDescription("Description: " + "Bot dms a user with your message ");
-  
-            try {
-            let who = message.mentions.users.first()
-            if (message.mentions.users.size < 1) return message.channel.send(usage);
-            if (message.author.id == who.id) return message.channel.send(`:x: Well no you can't dm yourself.`);
-            message2 = args.slice(1).join(` `);
-            if (message2 >= 400) return message.channel.send(usage)
-                who.send('**Message from ' + message.author.username + '**: ' + message2)
-                    message.channel.send(`Sucessfully sent message to ${who.username}.`)
-            } catch (err) {
-                return;
-            }
-            })
-    }         
-     if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`Missing permissions to dm this user with the bot.`)
-    sql.get(`SELECT * FROM scores WHERE guildId ="${message.guild.id}"`).then(row => {
-    const prefixtouse = row.prefix
-    const usage = new Discord.RichEmbed()
-            .setColor(0x00A2E8)
-            .setThumbnail(client.user.avatarURL)
-            .setTitle("Command: " + prefixtouse + "dm")
-            .addField("Usage", prefixtouse + "dm @Someone <message>")
-            .addField("Example", prefixtouse + "dm @Dowin Hey can you help me with something?")
-            .setDescription("Description: " + "Bot dms a user with your message ");
-  
-            try {
-            let who = message.mentions.users.first()
-            if (message.mentions.users.size < 1) return message.channel.send(usage);
-            if (message.author.id == who.id) return message.channel.send(`:x: Well no you can't dm yourself.`);
-            message2 = args.slice(1).join(` `);
-            if (message2 >= 400) return message.channel.send(usage)
-                who.send('**Message from ' + message.author.username + '**: ' + message2)
-                    message.channel.send(`Sucessfully sent message to ${who.username}.`)
-            } catch (err) {
-                return;
-            }
-            })     
- 
+    const categoryID = "734314126318370816";
+
+    var userName = message.author.username;
+    var userDiscriminator = message.author.discriminator;
+
+    var ticketExist = false;
+
+    message.guild.channels.cache.forEach(channel => {
+
+        if (channel.name == "〚🎫〛" + userName.toLowerCase()) {
+            ticketExist = true;
+
+            message.reply("Er is al een channel");
+
+            return;
+        }
+
+    });
+
+    if (ticketExist) return;
+
+    message.channel.send(embed).then(msg => msg.delete({timeout: 4000}));
+
+    message.guild.channels.create("〚🔮〛" + message.guild.memberCount(), { type: 'text' }).then(
+        (createdChannel) => {
+            createdChannel.setParent(categoryID).then(
+                (settedParent) => {
+
+                    settedParent.updateOverwrite(message.guild.roles.cache.find(x => x.name === '@everyone'), {
+                        SEND_MESSAGES: false,
+                        VIEW_CHANNEL: true
+                    });
+
+                }
+            ).catch(err => {
+                message.channel.send("Oh jee, iets ging niet helemaal goed! (catch 1)"),
+                    console.log(err);
+            });
+        }
+    ).catch(err => {
+        message.channel.send("Oh jee, iets ging niet helemaal goed! (catch 2)"),
+            console.log(err);
+    });
+
+}
 
 module.exports.help = {
-    name: "DM"
+    name: "Membercount",
+    description: "dit is Onze ticket systeem."
 }
