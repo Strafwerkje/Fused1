cf65ff8058df359786d3fc96d9b82d5d3eaba1e0
Strafwerkje <66158426+Strafwerkje@users.noreply.github.com>
2020-08-08 22:28:23 +0200

    Update Ping.js

--- commands/Ping.js
+++ commands/Ping.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     const ping = new discord.MessageEmbed()
-    .setDescription(`🏓\`${Date.now() - message.createdTimestamp}\`ms`);
+    .setDescription(`🏓 Wow je slaat je bal\`${Date.now() - message.createdTimestamp}\` Meter ver weg!`);
 
 
     message.channel.send(ping);
